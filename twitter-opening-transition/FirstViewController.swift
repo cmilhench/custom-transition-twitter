35f9c439a1fb82138703f259631cc3c7a98f74b9
cmilhench <cmilhench@gmail.com>
2019-07-16 15:24:02 +0100

    Initial project load

---
+++ twitter-opening-transition/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  twitter-opening-transition
+//
+//  Created by Colin Milhench on 16/07/2019.
+//  Copyright © 2019 Colin Milhench. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ twitter-opening-transition/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  twitter-opening-transition
+//
+//  Created by Colin Milhench on 16/07/2019.
+//  Copyright © 2019 Colin Milhench. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
